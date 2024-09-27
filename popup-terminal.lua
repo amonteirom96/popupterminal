265ab367762c5ef2f08d97f02499cbbbd27f1545
Arthur Monteiro <amonteirom96@gmail.com>
2024-09-27 11:34:24 -0300

    Create popup-terminal.lua

---
+++ popup-terminal.lua
@@ -0,0 +1,29 @@
+local M = {}
+
+function M.setup()
+  vim.api.nvim_create_user_command('PopupTerminal', function()
+    local buf = vim.api.nvim_create_buf(false, true)
+    local width = math.floor(vim.o.columns * 0.8)
+    local height = math.floor(vim.o.lines * 0.8)
+
+    local opts = {
+      style = 'minimal',
+      relative = 'editor',
+      width = width,
+      height = height,
+      row = math.floor((vim.o.lines - height) / 2),
+      col = math.floor((vim.o.columns - width) / 2),
+      border = 'rounded',
+    }
+
+    vim.api.nvim_open_win(buf, true, opts)
+
+    vim.fn.termopen(vim.o.shell)
+    vim.cmd('startinsert')
+  end, {})
+
+  -- Define o atalho <leader>t para abrir o terminal em popup
+  vim.api.nvim_set_keymap('n', '<leader>t', ':PopupTerminal<CR>', { noremap = true, silent = true })
+end
+
+return M
